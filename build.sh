9210da4454c9cbf5a8e781a9ca53ebf6b2be19d9
Caio-CGodoy <2109745@aluno.univesp.br>
2023-05-01 20:00:51 -0300

    Deploy Vercel

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ echo "Migrating database..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
+
 echo "Collecting static files..."
 
 python3.9 manage.py collectstatic --noinput
