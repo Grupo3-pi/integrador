6a2e220286161734409b1b06f3c7facaeaea06ec
Caio-CGodoy <2109745@aluno.univesp.br>
2023-05-01 19:32:00 -0300

    Deploy Vercel

--- build.sh
+++ build.sh
@@ -13,6 +13,6 @@ python3.9 manage.py migrate --noinput
 
 echo "Collecting static files..."
 
-python manage.py collectstatic
-
 python3.9 manage.py collectstatic --noinput
+
+python manage.py collectstatic --noinput
