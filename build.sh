32a51fee48e7c7465e5ca5dca2c390b47347721c
Caio-CGodoy <2109745@aluno.univesp.br>
2023-05-01 19:38:31 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,3 @@ echo "Migrating database..."
 
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-
-echo "Collecting static files..."
-
-python3.9 manage.py collectstatic --noinput
