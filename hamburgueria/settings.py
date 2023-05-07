f895c54699f1d1802bf5fb90d3050b03c12c3032
Caio-CGodoy <2109745@aluno.univesp.br>
2023-05-07 20:40:28 -0300

    Update Front-end

--- hamburgueria/settings.py
+++ hamburgueria/settings.py
@@ -64,6 +64,16 @@ WSGI_APPLICATION = 'hamburgueria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.postgresql',
+ #       'NAME': 'cardapio_postgre',
+ #       'USER': 'grupo3_ti_pi',
+ #       'PASSWORD': '4PuA2Jc9O0xVuvRf70UJDIWa1pAiBmrR',
+#        'HOST': 'dpg-ch81s0o2qv2864qb6n60-a.oregon-postgres.render.com',
+#    }
+#}
+
 DATABASES = {
     'default': dj_database_url.config(),
 }
@@ -104,12 +114,28 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+   BASE_DIR / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+
+
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+#STATIC_ROOT = os.path.join('static')
+
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+
+
+
+
 
 
 # Default primary key field type
