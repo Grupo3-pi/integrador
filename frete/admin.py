671a092e1d42023353d214bcef59bf47281fdfff
Caio-CGodoy <2109745@aluno.univesp.br>
2023-10-29 17:51:30 -0300

    Update App Frete

--- frete/admin.py
+++ frete/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Hamburgueria, FaixaFrete
 
-# Register your models here.
+admin.site.site_header = "Painel Administrativo"
+admin.site.index_title = "Hangar 252"
+admin.site.site_title = "Painel Administrativo"
+
+
+@admin.register(Hamburgueria)
+class HamburgueriaAdmin(admin.ModelAdmin):
+    list_display=('nome','latitude','longitude')
+
+
+@admin.register(FaixaFrete)
+class FaixaFreteAdmin(admin.ModelAdmin):
+    list_display=('hamburgueria','distancia_minima','distancia_maxima','valor_frete')
